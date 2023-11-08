f99857d341da14156ec1849ae1d901aa03dfa367
Dravya Gohil <dravyagohil@gmail.com>
2023-11-08 15:13:50 +0530

    lecture 4

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp._internal();
+
+  static const MyApp instance = MyApp._internal();
+
+  factory MyApp() => instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
