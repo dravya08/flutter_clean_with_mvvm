aa33fb3fec442c86ac924deedd957aff25ac7857
DravyaGohil <dravyagohil@gmail.com>
2023-12-05 10:35:28 +0530

    lecture 13

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -7,48 +7,56 @@ import 'value_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData().copyWith(
-    colorScheme: ColorScheme(
-      primary: ColorManager.primary,
-      onPrimary: ColorManager.primaryOpacity70,
-      background: ColorManager.white,
-      onBackground: ColorManager.black,
-      secondary: ColorManager.grey,
-      onSecondary: ColorManager.white,
-      error: ColorManager.error,
-      onError: ColorManager.white,
-      surface: ColorManager.white,
-      onSurface: ColorManager.black,
-      brightness: Brightness.light,
-    ),
-    disabledColor: ColorManager.grey1,
+      colorScheme: ColorScheme(
+        primary: ColorManager.primary,
+        onPrimary: ColorManager.primaryOpacity70,
+        background: ColorManager.white,
+        onBackground: ColorManager.black,
+        secondary: ColorManager.grey,
+        onSecondary: ColorManager.white,
+        error: ColorManager.error,
+        onError: ColorManager.white,
+        surface: ColorManager.white,
+        onSurface: ColorManager.black,
+        brightness: Brightness.light,
+      ),
+      disabledColor: ColorManager.grey1,
 
-    // card view theme
-    cardTheme: CardTheme(
-        color: ColorManager.white,
-        shadowColor: ColorManager.grey,
-        elevation: AppSize.s4),
-    // App bar theme
-    appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManager.primary,
-        elevation: AppSize.s4,
-        shadowColor: ColorManager.primaryOpacity70,
-        titleTextStyle:
-            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16)),
+      // card view theme
+      cardTheme: CardTheme(
+          color: ColorManager.white,
+          shadowColor: ColorManager.grey,
+          elevation: AppSize.s4),
+      // App bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primaryOpacity70,
+          titleTextStyle: getRegularStyle(
+              color: ColorManager.white, fontSize: FontSize.s16)),
 
-    // Button theme
-    buttonTheme: ButtonThemeData(
-        shape: const StadiumBorder(),
-        disabledColor: ColorManager.grey1,
-        buttonColor: ColorManager.primary,
-        splashColor: ColorManager.primaryOpacity70),
+      // Button theme
+      buttonTheme: ButtonThemeData(
+          shape: const StadiumBorder(),
+          disabledColor: ColorManager.grey1,
+          buttonColor: ColorManager.primary,
+          splashColor: ColorManager.primaryOpacity70),
 
-    // elevated button theme
-    elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-            textStyle: getRegularStyle(color: ColorManager.white),
-            backgroundColor: ColorManager.primary,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(AppSize.s12)))),
-  );
+      // elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              textStyle: getRegularStyle(color: ColorManager.white),
+              backgroundColor: ColorManager.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(AppSize.s12)))),
+
+      // Text theme
+      textTheme: TextTheme(
+          displayLarge: getSemiBoldStyle(
+              color: ColorManager.darkGrey, fontSize: FontSize.s16),
+          titleMedium: getMediumStyle(
+              color: ColorManager.lightGrey, fontSize: FontSize.s14),
+          bodySmall: getRegularStyle(color: ColorManager.grey1),
+          bodyLarge: getRegularStyle(color: ColorManager.grey)));
 }
