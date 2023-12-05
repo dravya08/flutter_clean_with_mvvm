f0818e2a7b430aab8a0cabc6f89d796be346325b
DravyaGohil <dravyagohil@gmail.com>
2023-12-05 10:33:39 +0530

    lecture 12

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -7,6 +7,19 @@ import 'value_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData().copyWith(
+    colorScheme: ColorScheme(
+      primary: ColorManager.primary,
+      onPrimary: ColorManager.primaryOpacity70,
+      background: ColorManager.white,
+      onBackground: ColorManager.black,
+      secondary: ColorManager.grey,
+      onSecondary: ColorManager.white,
+      error: ColorManager.error,
+      onError: ColorManager.white,
+      surface: ColorManager.white,
+      onSurface: ColorManager.black,
+      brightness: Brightness.light,
+    ),
     disabledColor: ColorManager.grey1,
 
     // card view theme
@@ -22,18 +35,20 @@ ThemeData getApplicationTheme() {
         shadowColor: ColorManager.primaryOpacity70,
         titleTextStyle:
             getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16)),
-    colorScheme: ColorScheme(
-      primary: ColorManager.primary,
-      onPrimary: ColorManager.primaryOpacity70,
-      background: ColorManager.white,
-      onBackground: ColorManager.black,
-      secondary: ColorManager.grey,
-      onSecondary: ColorManager.white,
-      error: ColorManager.error,
-      onError: ColorManager.white,
-      surface: ColorManager.white,
-      onSurface: ColorManager.black,
-      brightness: Brightness.light,
-    ),
+
+    // Button theme
+    buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.primaryOpacity70),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            textStyle: getRegularStyle(color: ColorManager.white),
+            backgroundColor: ColorManager.primary,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(AppSize.s12)))),
   );
 }
