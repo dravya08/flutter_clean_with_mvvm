e1419d6fd5e8a656b98673a2aabf52687a3eb41c
DravyaGohil <dravyagohil@gmail.com>
2023-12-05 10:31:00 +0530

    lecture 10

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'color_manager.dart';
+import 'value_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData().copyWith(
+    disabledColor: ColorManager.grey1,
+
+    // card view theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
+    // App bar theme
     colorScheme: ColorScheme(
       primary: ColorManager.primary,
       onPrimary: ColorManager.primaryOpacity70,
