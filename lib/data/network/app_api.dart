7e384020d2b6b65b7e98a3da07b3e2f24c74e7e5
DravyaGohil <dravyagohil@gmail.com>
2023-12-06 12:09:58 +0530

    lecture 47

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../responses.dart';
+import 'constant.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login();
+}
