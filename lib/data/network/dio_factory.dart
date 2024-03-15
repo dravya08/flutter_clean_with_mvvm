6db994b539575e9820be2ec474cc78832efcaa35
DravyaGohil <dravyagohil@gmail.com>
2024-03-15 15:12:46 +0530

    lecture 62

---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+
+import '../../app/constant.dart';
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content-type";
+const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "language";
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+    int _timeOut = 60 * 1000; // 1 min
+    Map<String, String> headers = {
+      CONTENT_TYPE: APPLICATION_JSON,
+      ACCEPT: APPLICATION_JSON,
+      AUTHORIZATION: Constant.token,
+      DEFAULT_LANGUAGE: "en" // todo get lang from app prefs
+    };
+
+    dio.options = BaseOptions(
+        baseUrl: Constant.baseUrl,
+        connectTimeout: Duration(minutes: _timeOut),
+        receiveTimeout: Duration(minutes: _timeOut),
+        headers: headers);
+
+    return dio;
+  }
+}
