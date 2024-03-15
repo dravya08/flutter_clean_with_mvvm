2f108c0c838dfc77fbd8b1bfb7a968ef20c50a24
DravyaGohil <dravyagohil@gmail.com>
2024-03-15 11:57:27 +0530

    lecture 55

---
+++ lib/data/repository/repository_impl.dart
@@ -0,0 +1,40 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_with_mvvm/data/mapper/mapper.dart';
+
+import '../../domain/model.dart';
+import '../../domain/repository.dart';
+import '../data_source/remote_data_source.dart';
+import '../network/failure.dart';
+import '../network/network_info.dart';
+import '../request/request.dart';
+
+class RepositoryImpl extends Repository {
+  final RemoteDataSource _remoteDataSource;
+  final NetworkInfo _networkInfo;
+
+  RepositoryImpl(this._remoteDataSource, this._networkInfo);
+
+  @override
+  Future<Either<Failure, Authentication>> login(
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      // its safe to call the API
+      final response = await _remoteDataSource.login(loginRequest);
+
+      if (response.status == 0) // success
+      {
+        // return data (success)
+        // return right
+        return Right(response.toDomain());
+      } else {
+        // return biz logic error
+        // return left
+        return Left(Failure(
+            409, response.message ?? "we have biz error logic from API side"));
+      }
+    } else {
+      // return connection error
+      return Left(Failure(501, "please check your internet connection"));
+    }
+  }
+}
