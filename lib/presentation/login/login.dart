141fde129056107c712a945d3a90c03e88e2d733
DravyaGohil <dravyagohil@gmail.com>
2024-03-18 17:06:18 +0530

    lecture 77

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../resources/assets_manager.dart';
+import '../resources/color_manager.dart';
+import '../resources/value_manager.dart';
 import 'login_viewmodel.dart';
 
 class LoginView extends StatefulWidget {
@@ -15,6 +19,7 @@ class _LoginViewState extends State<LoginView> {
 
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start();
@@ -38,6 +43,39 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SvgPicture.asset(ImageAssets.loginIc),
+                const SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outputIsUserNameValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _userNameController,
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
