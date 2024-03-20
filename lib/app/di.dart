d2ca986b7a98269efae81e991f9ee5ff2bfa7d0e
DravyaGohil <dravyagohil@gmail.com>
2024-03-20 10:44:44 +0530

    lecture 84

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,6 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../data/data_source/remote_data_source.dart';
+import '../data/network/app_api.dart';
+import '../data/network/dio_factory.dart';
+import '../data/network/network_info.dart';
+import '../data/repository/repository_impl.dart';
+import '../domain/repository/repository.dart';
 import 'app_prefs.dart';
 
 final instance = GetIt.instance;
@@ -14,4 +21,23 @@ Future<void> initAppModule() async {
   // app prefs instance
   instance
       .registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+
+  // network info
+  instance.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(InternetConnectionChecker()));
+
+  // dio factory
+  instance.registerLazySingleton<DioFactory>(() => DioFactory(instance()));
+
+  // app  service client
+  final dio = await instance<DioFactory>().getDio();
+  instance.registerLazySingleton<AppServiceClient>(() => AppServiceClient(dio));
+
+  // remote data source
+  instance.registerLazySingleton<RemoteDataSource>(
+      () => RemoteDataSourceImplementer(instance()));
+
+  // repository
+  instance.registerLazySingleton<Repository>(
+      () => RepositoryImpl(instance(), instance()));
 }
--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -10,8 +10,8 @@ import '../network/network_info.dart';
 import '../request/request.dart';
 
 class RepositoryImpl extends Repository {
-  RemoteDataSource _remoteDataSource;
-  NetworkInfo _networkInfo;
+  final RemoteDataSource _remoteDataSource;
+  final NetworkInfo _networkInfo;
 
   RepositoryImpl(this._remoteDataSource, this._networkInfo);
 
