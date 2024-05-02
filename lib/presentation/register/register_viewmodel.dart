ba7b7f858bdbb55df522f42d71661655d0ed4bf0
DravyaGohil <dravyagohil@gmail.com>
2024-05-02 16:07:57 +0530

    lecture 121

---
+++ lib/presentation/register/register_viewmodel.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'dart:io';
+
+import '../base/baseviewmodel.dart';
+
+class RegisterViewModel extends BaseViewModel {
+  final StreamController _userNameStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _mobileNumberStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _emailStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _passwordStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _profilePictureStreamController =
+      StreamController<File>.broadcast();
+
+  final StreamController _isAllInputsValidStreamController =
+      StreamController<void>.broadcast();
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  void dispose() {
+    _isAllInputsValidStreamController.close();
+    _userNameStreamController.close();
+    _mobileNumberStreamController.close();
+    _emailStreamController.close();
+    _passwordStreamController.close();
+    _profilePictureStreamController.close();
+    super.dispose();
+  }
+}
