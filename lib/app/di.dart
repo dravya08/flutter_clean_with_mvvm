8d5ac12290abe0b4e1f41c4d0437f94a2d73bb65
DravyaGohil <dravyagohil@gmail.com>
2024-05-06 17:15:06 +0530

    lecture 129

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -10,8 +10,10 @@ import '../data/repository/repository_impl.dart';
 import '../domain/repository/repository.dart';
 import '../domain/usecase/forgot_password_usecase.dart';
 import '../domain/usecase/login_usecase.dart';
+import '../domain/usecase/register_usecase.dart';
 import '../presentation/forgot_password/forgot_password_viewmodel.dart';
 import '../presentation/login/login_viewmodel.dart';
+import '../presentation/register/register_viewmodel.dart';
 import 'app_prefs.dart';
 
 final instance = GetIt.instance;
@@ -59,5 +61,14 @@ initForgotPasswordModule() {
         () => ForgotPasswordUseCase(instance()));
     instance.registerFactory<ForgotPasswordViewModel>(
         () => ForgotPasswordViewModel(instance()));
+  }
+}
+
+initRegisterModule() {
+  if (!GetIt.I.isRegistered<RegisterUseCase>()) {
+    instance
+        .registerFactory<RegisterUseCase>(() => RegisterUseCase(instance()));
+    instance.registerFactory<RegisterViewModel>(
+        () => RegisterViewModel(instance()));
   }
 }
--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_with_mvvm/presentation/register/register_viewmodel.dart';
+
+import '../../app/di.dart';
 
 class RegisterView extends StatefulWidget {
-  const RegisterView({Key? key}) : super(key: key);
+  const RegisterView({super.key});
 
   @override
-  _RegisterViewState createState() => _RegisterViewState();
+  State<RegisterView> createState() => _RegisterViewState();
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  RegisterViewModel _viewModel = instance<RegisterViewModel>();
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController _userNameTextEditingController =
+      TextEditingController();
+  TextEditingController _mobileNumberTextEditingController =
+      TextEditingController();
+  TextEditingController _userEmailEditingController = TextEditingController();
+  TextEditingController _userPasswordEditingController =
+      TextEditingController();
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+  _bind() {
+    _viewModel.start();
+    _userNameTextEditingController.addListener(() {
+      _viewModel.setUserName(_userNameTextEditingController.text);
+    });
+
+    _userPasswordEditingController.addListener(() {
+      _viewModel.setPassword(_userPasswordEditingController.text);
+    });
+
+    _userEmailEditingController.addListener(() {
+      _viewModel.setEmail(_userEmailEditingController.text);
+    });
+
+    _mobileNumberTextEditingController.addListener(() {
+      _viewModel.setMobileNumber(_mobileNumberTextEditingController.text);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
@@ -15,7 +54,7 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _viewModel.dispose();
     super.dispose();
   }
 }
